4ddd0822d110dafcc7580aad2883299fcf248091
Luis Eduardo Alcantara Olvera <geekluis@MacBook-Pro-de-Luis.local>
2021-06-12 01:13:21 -0500

    Se creo el metodo para registrar usuarios y retornar token de acceso

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Http\Request;
+use App\Models\User;
+
+class AuthController extends Controller
+{
+    public function register( Request $request ){
+        // dd( $request->all() );
+        
+        $validatedData = $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:8',
+        ]);
+        
+        
+        $user = User::create([
+            'name' => $validatedData['name'],
+            'email' => $validatedData['email'],
+            'password' => Hash::make($validatedData['password']),
+        ]);
+
+        $token = $user->createToken('aut_token')->plainTextToken;
+
+        return response()->json([
+            'acess_token' => $token,
+            'token_type' => 'Bearer'
+        ]);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,9 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\{
+    AuthController
+};
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -17,3 +20,5 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('/register', [ AuthController::class, 'register' ] );
